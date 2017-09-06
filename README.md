6df4c530e936df433d872d3ef8629cb702672860
Sirakorn Lamyai <tanersirakorn@gmail.com>
2017-09-06 10:13:29 +0700

    :memo: Edited README

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# 2017-204111
-Resources for Computers and Programming Sec 1, 2017.
+# Resouces for 01204111-Sec1-2017
+
+Here are codes, resources, and snippets used in demo/helpdesk session of 01204111 Section 1, Department of Computer Engineering, Kasetsart University.
---
+++ matrix_muitlply.py
@@ -0,0 +1,23 @@
+import numpy
+
+rows_a = int(input("Rows of matrix a: "))
+print("Input matrix a:")
+a = []
+for i in range(rows_a):
+    a.append([float(x) for x in input().split()])
+rows_b = int(input("Rows of matrix b: "))
+print("Input matrix b:")
+b = []
+for i in range(rows_b):
+    b.append([float(x) for x in input().split()])
+
+try:
+    a_times_b = numpy.dot(a,b)
+
+    print()
+    for i in a_times_b:
+        for j in i:
+            print("{:<10.2f}".format(j) if int(j)==j else "{:<10.0f}".format(j),end='')
+        print()
+except ValueError:
+    print("The matrices dimension resulted error in multiplication.")
